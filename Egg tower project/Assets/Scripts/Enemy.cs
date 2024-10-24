66211264af48a8dfafec2037bb7e820127671896
DragonH0rnet <benmphillips1@gmail.com>
2024-10-24 23:35:28 +0100

    Basic wave + base tower and enemy

--- Egg tower project/Assets/Scripts/Enemy.cs
+++ Egg tower project/Assets/Scripts/Enemy.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bb5ac18ce0a97346f244efe8f7ab6f0b4b5e3133ea00ba93fde0f44c22c22ea4
-size 1601
+oid sha256:37f58a446291e52fefe2dbb24c67f01adcd2ecfac2c2f321b1a5e36019374ec6
+size 1630
---
+++ Egg tower project/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8cb124aa3a2150f696f7e2ea18ca96b2497b77a7ccf1e8e28bfea4402e3e855f
+size 920
