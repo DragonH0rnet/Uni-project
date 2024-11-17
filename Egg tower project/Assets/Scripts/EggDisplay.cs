47422b1937b2da11948bef92f07ff044988c3a0d
lunaviadev <kinab307.212@gmail.com>
2024-11-17 19:48:06 +0000

    Healthbar + Egg inclusion // Minor changes

---
+++ Egg tower project/Assets/Scripts/EggDisplay.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:54753008f272d2700bbf86d09442dc243047dc17d34397c6693e10a53bee0320
+size 742
---
+++ Egg tower project/Assets/Scripts/EggScript.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13cd1b743880295e296982a0206fd4fa09f05d930b64c24d2be25c1b48248c80
+size 1158
